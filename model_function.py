64026b1653a457c62b6fe97496439ba33a45ae38
Kevin Liu <88750544+kevinliu399@users.noreply.github.com>
2023-11-18 22:20:00 -0500

    Update model_function.py

--- model_function.py
+++ model_function.py
@@ -1,13 +1,14 @@
 from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
-import cv2
 
 loaded_model = load_model('my_digit_symbol_model.keras')
 
 # Function to preprocess and predict new images
 def predict_image():
     image = Image.open(r'mask_capture.jpeg').convert('L')
+
+    # pre process
     image = image.resize((28, 28))
     image_array = np.array(image) / 255.0
     image_array = image_array.reshape(-1, 28, 28, 1)
@@ -15,6 +16,5 @@ def predict_image():
 
     return np.argmax(prediction)
 
-
-result = predict_image()
-print("Predicted Label:", result)
+#result = predict_image()
+#print("Predicted Label:", result)
