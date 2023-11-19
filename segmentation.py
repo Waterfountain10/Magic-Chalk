e1c24af818718f36a97e1590a3953dce30c5eff3
Kevin Liu <88750544+kevinliu399@users.noreply.github.com>
2023-11-18 21:49:22 -0500

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -7,27 +7,22 @@ model = load_model('my_digit_symbol_model.keras')
 
 
 def segment_and_predict(image_path = 'mask_capture.jpeg'):
-    # Read the image and convert to grayscale
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Apply thresholding for black text on white background
     _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-    # Invert the thresholded image for dilation (since text is now black)
     inverted_thresh = cv2.bitwise_not(thresh)
 
-    # Apply dilation to close gaps in characters
     kernel = np.ones((3, 3), np.uint8)
     dilated = cv2.dilate(inverted_thresh, kernel, iterations=1)
 
-    # Find contours on the dilated image
     contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     predictions = []
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        # Crop from the original thresholded image (not the dilated one)
+
         cropped_char = thresh[y:y+h, x:x+w]
         resized_char = cv2.resize(cropped_char, (28, 28))
 
@@ -43,7 +38,7 @@ def segment_and_predict(image_path = 'mask_capture.jpeg'):
         #cv2.waitKey(0)
 
     #cv2.destroyAllWindows()
-    predictions[0], predictions[1] = predictions[1], predictions[0]
+    
 
     return predictions
 
