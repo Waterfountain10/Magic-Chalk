0c5fccb1c0429d4d1488b57ccb74f15d948716d1
Kevin Liu <88750544+kevinliu399@users.noreply.github.com>
2023-11-19 02:13:02 -0500

    Update test_model.py
    latest results

--- test_model.py
+++ test_model.py
@@ -4,5 +4,5 @@ from tensorflow.keras.models import load_model
 loaded_model = load_model('my_digit_symbol_model.keras')
 test_loss, test_accuracy = loaded_model.evaluate(test_images, test_labels)
 
-print("Test Loss:", test_loss) #0.96
-print("Test Accuracy:", test_accuracy) #0.17
+print("Test Loss:", test_loss) #0.97
+print("Test Accuracy:", test_accuracy) #0.15
